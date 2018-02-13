31eccb624b7edbfa20bce295b2e1e0a50cac2af1
Shipu Wang <shipu.wsp@gmail.com>
2018-02-13 03:41:34 -0800

    Get data from API

---
+++ Dark Sky/Model/DailyModel.swift
@@ -0,0 +1,13 @@
+//
+//  DailyModel.swift
+//  Dark Sky
+//
+//  Created by Shipu Wang on 2/13/18.
+//  Copyright © 2018 Shipu Wang. All rights reserved.
+//
+
+import UIKit
+
+class DailyModel: BaseModel {
+
+}
