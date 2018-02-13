f8478b7ad0c908dd5f32559552f60deb47e9bb2d
Shipu Wang <shipu.wsp@gmail.com>
2018-02-12 20:58:39 -0800

    base architecture

---
+++ Dark Sky/Model/BaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseModel.swift
+//  Dark Sky
+//
+//  Created by Shipu Wang on 2/12/18.
+//  Copyright © 2018 Shipu Wang. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'Dark Sky' do
+
+pod 'Alamofire', '~> 4.5'
+
+use_frameworks!
+
+end
+
